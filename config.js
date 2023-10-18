0988075a49b87a7b4c923ca2ad3f0cb35801180a
iansexton <mydesignguy@gmail.com>
2023-10-17 21:50:47 -0400

    ga-updated database credentials to correct db lol

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
-
-
-
-const DATABASE_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tnd5cmm.mongodb.net/mongoburgers?retryWrites=true&w=majority`;
+const DATABASE_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tnd5cmm.mongodb.net/nomad?retryWrites=true&w=majority`;
 
 const PORT = process.env.PORT || 2023;
 
-module.exports = {DATABASE_URL, PORT}
+module.exports = { DATABASE_URL, PORT };
