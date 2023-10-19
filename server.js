42e17c669d875ed81de1ec36780db671b9d12571
iansexton <mydesignguy@gmail.com>
2023-10-18 21:39:50 -0400

    ga-setting up server.js for initial running

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const server = express();
+const ejsLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const path = require('path');
 const port = 2023;
+const router = express.Router();
+const { DATABASE_URL, PORT } = require('./config.js');
 
 server.set('view engine', 'ejs');
+server.use(ejsLayouts);
+
+//--------------  Import Models  ----------------//
+
+//--------------  Import CSS &/or JSON ----------------//
+
+server.use(express.static(path.join(__dirname, '/public')));
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+//--------------  Start Server  ----------------//
+
+const startServer = async () => {
+  // connect to to DB
+  await mongoose.connect(DATABASE_URL);
+
+  mongoose.connection.on('connected', () => {
+    console.log('Connected to ' + DATABASE_URL);
+  });
+};
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+//--------------  Routes Middleware  ----------------//
 
 server.get('/', (req, res) => {
-    res.render('travelplan');
+  res.render('travelplan');
 });
-
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
