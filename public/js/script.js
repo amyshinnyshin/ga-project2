3ca33ce467f5c59b4a775c6d2b0f9cf0ae48b9b5
amyshinnyshin@gmail.com <amyshinnyshin@gmail.com>
2023-10-23 10:40:56 -0500

    Modal CSS Updates

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,15 @@
+// ---------------------------------------VARIABLES
 const currentPage = window.location.pathname;
 const navButtons = document.querySelectorAll(".navigation-menu li");
+
+//-----MODAL VARIABLES
+const addEventButton = document.getElementById('addEventButton');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeModalButton = modalOverlay.querySelector('.close');
+const cancelModalButton = modalOverlay.querySelector('.template-modal footer .cancel');
+const saveChangeButton = modalOverlay.querySelector('.template-modal-footer .save-changes');
+
+
 
 navButtons.forEach(navButton => {
     const navCopy = navButton.textContent
@@ -7,4 +17,17 @@ navButtons.forEach(navButton => {
         navButton.classList.add('active-nav-button');
     }
 })
+
+//-----OPEN & CLOSE MODALS
+function openModal() {
+    modalOverlay.style.display = 'block';
+}
+
+function closeModal() {
+    modalOverlay.style.display = 'none';
+}
+
+addEventButton.addEventListener('click', openModal);
+closeModalButton.addEventListener('click', closeModal);
+cancelModalButton.addEventListener('click', closeModal)
 
