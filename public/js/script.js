4105393838c5ab65711a973e941a3cc7a3c86bd9
iansexton <mydesignguy@gmail.com>
2023-10-28 12:20:27 -0400

    Working on updating user and adding dashboard as /

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 // ---------------------------------------VARIABLES
 const currentPage = window.location.pathname;
-const navButtons = document.querySelectorAll(".navigation-menu li");
+const navButtons = document.querySelectorAll('.navigation-menu li');
 
 //-----MODAL VARIABLES
 const addEventButton = document.getElementById('addEventButton');
@@ -9,26 +9,22 @@ const closeModalButton = modalOverlay.querySelector('.close');
 const cancelModalButton = modalOverlay.querySelector('.cancel');
 const saveChangeButton = modalOverlay.querySelector('.save-changes');
 
-
-
-navButtons.forEach(navButton => {
-    const navCopy = navButton.textContent
-    if (navCopy.contains('My Plans') && (currentPage === '/' || currentPage === '/travelplan')) {
-        navButton.classList.add('active-nav-button');
-    }
-})
+navButtons.forEach((navButton) => {
+  const navCopy = navButton.textContent;
+  if (navCopy.contains('My Plans') && (currentPage === '/' || currentPage === '/travelplan')) {
+    navButton.classList.add('active-nav-button');
+  }
+});
 
 //-----OPEN & CLOSE MODALS
 function openModal() {
-    modalOverlay.style.display = 'block';
+  modalOverlay.style.display = 'block';
 }
 
 function closeModal() {
-    modalOverlay.style.display = 'none';
+  modalOverlay.style.display = 'none';
 }
 
 addEventButton.addEventListener('click', openModal);
 closeModalButton.addEventListener('click', closeModal);
 cancelModalButton.addEventListener('click', closeModal);
-
-
