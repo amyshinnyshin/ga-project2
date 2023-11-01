6406d8e7aa4ef465c614e9d02949a39ee50e01be
amyshinnyshin@gmail.com <amyshinnyshin@gmail.com>
2023-10-31 19:58:00 -0500

    changes

--- public/js/script.js
+++ public/js/script.js
@@ -9,21 +9,23 @@ const closeModalButton = modalOverlay.querySelector('.close');
 const cancelModalButton = modalOverlay.querySelector('.cancel');
 const saveChangeButton = modalOverlay.querySelector('.save-changes');
 
-navButtons.forEach((navButton) => {
-  const navCopy = navButton.textContent;
-  if (navCopy.contains('My Plans') && (currentPage === '/' || currentPage === '/travelplan')) {
-    navButton.classList.add('active-nav-button');
-  }
-});
 
-//-----OPEN & CLOSE MODALS
-function openModal() {
-  modalOverlay.style.display = 'block';
-}
 
-function closeModal() {
-  modalOverlay.style.display = 'none';
-}
+navButtons.forEach(navButton => {
+    const navCopy = navButton.textContent
+    if (navCopy.contains('My Plans') && (currentPage === '/' || currentPage === '/travelplan')) {
+        navButton.classList.add('active-nav-button');
+    }
+})
+
+// //-----OPEN & CLOSE MODALS
+// function openModal() {
+//     modalOverlay.style.display = 'block';
+// }
+
+// function closeModal() {
+//     modalOverlay.style.display = 'none';
+// }
 
 addEventButton.addEventListener('click', openModal);
 closeModalButton.addEventListener('click', closeModal);
--- server.js
+++ server.js
@@ -3,16 +3,10 @@ const app = express();
 const ejsLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const path = require('path');
-const port = 2023;
 const router = express.Router();
 const { DATABASE_URL, PORT } = require('./config.js');
 
-const {} = require('./controllers/dashboardController');
 const { travelplans } = require('./controllers/travelPlansController.js');
-
-const {} = require('./controllers/dashboardController.js');
-
-const {} = require('./controllers/dashboardController.js');
 
 //app.set('view engine', 'ejs');
 //app.use(ejsLayouts);
@@ -26,10 +20,7 @@ app.use(express.static('public'));
 console.log('dirname', __dirname);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
-// app.use('/bootstrap-icons', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/font/')));
-// app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
-// app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+
 
 //--------------  Start Server  ----------------//
 
@@ -42,7 +33,7 @@ const startServer = async () => {
   });
 
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${PORT}`);
   });
   app.on('connected', () => {
     console.log('MongoDB connected on:', DATABASE_URL);
@@ -55,7 +46,6 @@ const travelPlansRouter = require('./routers/travelPlansRouter.js');
 const usersRouter = require('./routers/usersRouter.js');
 const dashboardRouter = require('./routers/dashboardRouter.js');
 
-app.use('/users', dashboardRouter);
 app.use('/travelplans', travelPlansRouter);
 
 app.use('/users', usersRouter);
