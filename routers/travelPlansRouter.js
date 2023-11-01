407750e87292c230e2dbab80d322f3b6f18a3d8e
iansexton <mydesignguy@gmail.com>
2023-11-01 19:22:17 -0400

    new travel page comes up now

--- routers/travelPlansRouter.js
+++ routers/travelPlansRouter.js
@@ -1,46 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const { Plan } = require('../models/plan');
-const { travelplans } = require('../controllers/travelPlansController');
+const { travelplans, allPlans } = require('../controllers/travelPlansController');
 
-// GET travelplan by id route
-router.get('/:id', async (req, res) => {
-  const travelPlans = await Plan.findById(req.params.id);
-  res.render('newTravelPlanForm.ejs', { travelPlan });
-});
+// // GET travelplan by id route
+// router.get('/:id', async (req, res) => {
+//   const travelPlans = await Plan.findById(req.params.id);
+//   res.render('newTravelPlanForm.ejs', { travelPlan });
+// });
+//
+// // // GET all travelplans route
+// // router.get('/', async (req, res) => {
+// //   const travelPlans = await Plan.find();
+// //   res.render('server.ejs', { travelPlans });
+// // });
+//
+// // CREATE - Add a new travel plan to the database
+// router.post('/', async (req, res) => {
+//   console.log('You have reached the POST travelPlansRouter');
+//   console.log(req.body);
+//   const newPlan = await Plan.create(req.body);
+//   res.json(newPlan);
+//
+//   // send to browser acknowledgement: redirect to homepage (with note?)
+//
+//   // UPDATE travelPlan by id route
+//   router.put('/:id', async (req, res) => {
+//     await Plan.findByIdAndUpdate(req.params.id, req.body);
+//     res.redirect('/travelplans/' + req.params.id);
+//   });
+//
+//   // DELETE travel by id route
+//   router.delete('/:id', async (req, res) => {
+//     await Plan.findByIdAndRemove(req.params.id);
+//     res.redirect('/travelplans');
+//   });
+//
+// });
 
-// GET all travelplans route
-router.get('/', async (req, res) => {
-  const travelPlans = await Plan.find();
-  res.render('server.ejs', { travelPlans });
-});
+router.get('/new', travelplans);
 
-// CREATE - Add a new travel plan to the database
-router.post('/', async (req, res) => {
-  console.log('You have reached the POST travelPlansRouter');
-  console.log(req.body);
-  const newPlan = await Plan.create(req.body);
-  res.json(newPlan);
-
-  // send to browser acknowledgement: redirect to homepage (with note?)
-
-  // UPDATE travelPlan by id route
-  router.put('/:id', async (req, res) => {
-    await Plan.findByIdAndUpdate(req.params.id, req.body);
-    res.redirect('/travelplans/' + req.params.id);
-  });
-
-  // DELETE travel by id route
-  router.delete('/:id', async (req, res) => {
-    await Plan.findByIdAndRemove(req.params.id);
-    res.redirect('/travelplans');
-  });
-
-});
-
-router.get('/new', (req, res) => {
-  res.render('newTravelPlansForm.ejs');
-});
 
 // POST
 router.post('/:planId', async (req, res) => {
